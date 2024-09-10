cb1b70787aa03da4d6695c1d330d1b6e996af217
igorct1 <igorct77@gmail.com>
2024-09-10 14:27:27 -0300

    feat: adding docker and initial config

---
+++ src/test/java/com/example/desafio_jr_simplify/DesafioJuniorBackendSimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_jr_simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuniorBackendSimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
